8885884af3c8ba71c2a2cf5e500acd070a5618e4
grimshaw-a <58604535+grimshaw-a@users.noreply.github.com>
2020-09-01 09:17:26 -0600

    Update Client.java

--- Chapter31-Networking/Exercise31-01/Client.java
+++ Chapter31-Networking/Exercise31-01/Client.java
@@ -18,7 +18,7 @@ import javafx.event.ActionEvent;
 import java.net.*;
 
 public class Client extends Application {
-  // Text field for receiving radius
+  // Text fields for user input
   private TextField tfAnnualInterestRate = new TextField();
   private TextField tfNumOfYears = new TextField();
   private TextField tfLoanAmount = new TextField();
@@ -30,7 +30,7 @@ public class Client extends Application {
   @Override // Override the start method in the Application class
   public void start(Stage primaryStage) {
     ta.setWrapText(true);
-   
+    
     GridPane gridPane = new GridPane();
     gridPane.add(new Label("Annual Interest Rate"), 0, 0);
     gridPane.add(new Label("Number Of Years"), 0, 1);
@@ -52,7 +52,6 @@ public class Client extends Application {
     pane.setCenter(new ScrollPane(ta));
     pane.setTop(gridPane);
     
-    //btSubmit.setOnAction(new ButtonListener());
     btSubmit.setOnAction(e -> {  	
     	      try {  
     	        Socket socket = new Socket("localhost", 8000);
@@ -82,44 +81,10 @@ public class Client extends Application {
     
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane, 400, 250);
-    primaryStage.setTitle("Exercise31_01Client"); // Set the stage title
+    primaryStage.setTitle("Client"); // Set the stage title
     primaryStage.setScene(scene); // Place the scene in the stage
     primaryStage.show(); // Display the stage
   }
-  /*
-  private class ButtonListener implements EventHandler<ActionEvent> {
-	  ta.appendText("asdf");
-
-    @Override
-    public void handle(ActionEvent e) {   	
-      try {  
-        Socket socket = new Socket("localhost", 8000);
-        DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
-        ObjectInputStream fromServer = new ObjectInputStream(socket.getInputStream());
-        
-        double interest = Double.parseDouble(tfAnnualInterestRate.getText().trim());
-        int years = Integer.parseInt(tfNumOfYears.getText().trim());
-        double amount = Double.parseDouble(tfLoanAmount.getText().trim());
-       
-        toServer.writeDouble(interest);
-        toServer.writeInt(years);
-        toServer.writeDouble(amount);
-        
-        
-        while(true) {
-        	String inputString = (String)fromServer.readObject();
-        	ta.appendText("asdf");
-        	System.out.println(inputString);	
-        }
-      }
-      catch (IOException ex) {
-        ex.printStackTrace();
-      }
-      catch(ClassNotFoundException ex) {
-    	  ex.printStackTrace();
-      }
-    }
-  }*/
  
   /**
    * The main method is only needed for the IDE with limited
